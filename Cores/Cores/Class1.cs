83bcaf27f9c788789f78a2257d4a7bd716f5929f
Serkan SERTER <40948644+SerkanSerterTR@users.noreply.github.com>
2018-07-08 19:44:13 +0300

    Exceptions
    Exception with log methods

--- Cores/Cores/Class1.cs
+++ Cores/Cores/Class1.cs
@@ -4,32 +4,37 @@ using System.Text;
 using Cores.CoreExceptions;
 using Cores.CoreExtensions;
 
-
-
 namespace Cores
 {
     class Class1
     {
         public void Examp()
         {
-            Exceptions.ExceptionWithLog(Test, Log);
+            Exceptions.ThrowException(Test);
+            Exceptions.ThrowException(() => { throw new NotImplementedException(); });
+            Exceptions.ExceptionWithVoidLog(Test, Log);
+            Exceptions.ExceptionWithVoidLog(() => { throw new NotImplementedException(); },
+                (Exception ex) =>
+                {
+                    string msg = ex.Message;
+                    throw new NotImplementedException();
+                });
             var ss = new Class1();
             var ss2 = ss.xClone();
 
             int a = 1;
-            double b = a.xParse<int,double>();
-             b.is
+            double b = a.xParse<int, double>();
 
         }
 
-        private object Log(Exception arg)
+        private void Log(Exception arg)
         {
             throw new NotImplementedException();
         }
 
-        public object Test()
+        public void Test()
         {
-            return new object();
+            throw new NotImplementedException();
         }
     }
 }
--- Cores/Cores/CoreExceptions/Exceptions.cs
+++ Cores/Cores/CoreExceptions/Exceptions.cs
@@ -8,11 +8,11 @@ namespace Cores.CoreExceptions
         /// Gönderilen metotda Exception varsa Exception fırlatır.
         /// </summary>
         /// <param name="func"></param>
-        public static object ThrowException(Func<object> func)
+        public static void ThrowException(Action func)
         {
             try
             {
-                return func.Invoke();
+                func.Invoke();
             }
             catch (Exception ex)
             {
@@ -20,12 +20,27 @@ namespace Cores.CoreExceptions
             }
         }
 
+        /// <summary>
+        /// Gönderilen metotda Exception varsa Exception fırlatır.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void ThrowException<T>(Func<T> func)
+        {
+            try
+            {
+                func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
         /// <summary>
         /// Gönderilen metotda Exception varsa log metotunu çalıştırır ve Exception fırlatır.
         /// </summary>
         /// <param name="func"></param>
-        public static object ThrowExceptionWithLog(Func<object> func, Func<Exception, object> logFunc)
+        public static object ThrowExceptionWithLog<T>(Func<T> func, Func<Exception, object> logFunc)
         {
             try
             {
@@ -34,6 +49,23 @@ namespace Cores.CoreExceptions
             catch (Exception ex)
             {
                 return logFunc.Invoke(ex);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Gönderilen metotda Exception varsa log metotunu çalıştırır ve Exception fırlatır.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void ThrowExceptionWithLog(Action func, Func<Exception, object> logFunc)
+        {
+            try
+            {
+                func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                logFunc.Invoke(ex);
                 throw ex;
             }
         }
@@ -42,7 +74,7 @@ namespace Cores.CoreExceptions
         /// Gönderilen metotda Exception varsa log metotunu çalıştırır.
         /// </summary>
         /// <param name="func"></param>
-        public static object ExceptionWithLog(Func<object> func, Func<Exception, object> logFunc)
+        public static object ExceptionWithLog<T>(Func<T> func, Func<Exception, object> logFunc)
         {
             try
             {
@@ -51,6 +83,38 @@ namespace Cores.CoreExceptions
             catch (Exception ex)
             {
                 return logFunc.Invoke(ex);
+            }
+        }
+
+        /// <summary>
+        /// Gönderilen metotda Exception varsa log metotunu çalıştırır.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void ExceptionWithLog(Action func, Func<Exception, object> logFunc)
+        {
+            try
+            {
+                func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                logFunc.Invoke(ex);
+            }
+        }
+
+        /// <summary>
+        /// Gönderilen metotda Exception varsa log metotunu çalıştırır.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void ExceptionWithVoidLog(Action func, Action<Exception> logFuncVoid)
+        {
+            try
+            {
+                func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                logFuncVoid.Invoke(ex);
             }
         }
     }
