3f8a1ee7fc7199bf1ee7c8f2b18d9adedf67ab5b
Serkan SERTER <40948644+SerkanSerterTR@users.noreply.github.com>
2018-07-09 16:56:05 +0300

    Object Extensions
    Object Extensions

--- Cores/Cores/CoreExtensions/ObjectExtensions.cs
+++ Cores/Cores/CoreExtensions/ObjectExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Cores.CoreExtensions
 {
@@ -12,14 +13,95 @@ namespace Cores.CoreExtensions
         /// <returns></returns>
         public static T xClone<T>(this T obj) where T : class, new()
         {
-            T clone = new T(); 
+            T clone = new T();
             foreach (var property in obj.GetType().GetProperties())
             {
                 var propertyValue = property.GetValue(property.Name);
                 var clonePropertyInfo = clone.GetType().GetProperty(property.Name);
-                clonePropertyInfo.SetValue(clone, Convert.ChangeType(propertyValue, clonePropertyInfo.PropertyType), null);
+                if (clonePropertyInfo != null)
+                    clonePropertyInfo.SetValue(clone, Convert.ChangeType(propertyValue, clonePropertyInfo.PropertyType), null);
             }
             return clone;
+        }
+
+        /// <summary>
+        /// Nesnenin alan değerlerinin aynı sınıftan başka bir nesneye aktarılmasını sağlar.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mainObject"></param>
+        /// <returns></returns>
+        public static void xCopyProperties<T>(this T mainObject, ref T targetObject)
+            where T : class, new()
+        {
+            foreach (var property in mainObject.GetType().GetProperties())
+            {
+                var propertyValue = property.GetValue(property.Name);
+                var targetPropertyInfo = targetObject.GetType().GetProperty(property.Name);
+                if (targetPropertyInfo != null)
+                    targetPropertyInfo.SetValue(targetObject, Convert.ChangeType(propertyValue, targetPropertyInfo.PropertyType), null);
+            } 
+        }
+
+        /// <summary>
+        /// Nesnenin alan değerlerinin farklı sınıftan başka bir nesneye aktarılmasını sağlar.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mainObject"></param>
+        /// <returns></returns>
+        public static void xCopyProperties<TMain, TTarget>(this TMain mainObject, ref TTarget targetObject)
+            where TMain : class, new()
+            where TTarget : class, new()
+        {
+            foreach (var property in mainObject.GetType().GetProperties())
+            {
+                var propertyValue = property.GetValue(property.Name);
+                var targetPropertyInfo = targetObject.GetType().GetProperty(property.Name);
+                if (targetPropertyInfo != null)
+                    targetPropertyInfo.SetValue(targetObject, Convert.ChangeType(propertyValue, targetPropertyInfo.PropertyType), null);
+            } 
+        }
+
+        /// <summary>
+        /// Nesnenin alan değerlerinin aynı sınıftan başka bir nesne ile karşılaştırmasını yapar.
+        /// Tüm alan değerlernin eşit-aynı olaması beklenir.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mainObject"></param>
+        /// <returns></returns>
+        public static bool xCompareProperties<T>(this T mainObject, T targetObject)
+            where T : class, new()
+        {
+            foreach (var property in mainObject.GetType().GetProperties())
+            {
+                var propertyValue = property.GetValue(property.Name);
+                PropertyInfo targetPropertyInfo = targetObject.GetType().GetProperty(property.Name);
+                if (targetPropertyInfo != null)
+                    if (propertyValue != targetPropertyInfo.GetValue(targetObject))
+                        return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Nesnenin alan değerlerinin farklı sınıftan başka bir nesne ile karşılaştırmasını yapar.
+        /// Tüm alan değerlernin eşit-aynı olaması beklenir.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mainObject"></param>
+        /// <returns></returns>
+        public static bool xCompareProperties<TMain, TTarget>(this TMain mainObject, TTarget targetObject)
+            where TMain : class, new()
+            where TTarget : class, new()
+        {
+            foreach (var property in mainObject.GetType().GetProperties())
+            {
+                var propertyValue = property.GetValue(property.Name);
+                PropertyInfo targetPropertyInfo = targetObject.GetType().GetProperty(property.Name);
+                if (targetPropertyInfo != null)
+                    if (propertyValue != targetPropertyInfo.GetValue(targetObject))
+                        return false;
+            }
+            return true;
         }
     }
 }
